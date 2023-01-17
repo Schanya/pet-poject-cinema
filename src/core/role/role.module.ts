0f14060e35dc9639384c9a39bd192cbc6a97fc61
Schanya <anyashemelevadm@gmail.com>
2023-01-17 14:07:10 +0300

    add role module

---
+++ src/core/role/role.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+import { Role } from './role.entity';
+import { RoleController } from './role.controller';
+import { RoleService } from './role.service';
+
+@Module({
+	imports: [SequelizeModule.forFeature([Role])],
+	controllers: [RoleController],
+	providers: [RoleService],
+	exports: [RoleService],
+})
+export class RoleModule {}
---
+++ src/core/role/role.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { RoleDto } from './dto/role.dto';
+import { Role } from './role.entity';
+
+@Injectable()
+export class RoleService {
+	constructor(@InjectModel(Role) private roleRepository: typeof Role) {}
+
+	public async findBy(options: any): Promise<Role> {
+		const role = await this.roleRepository.findOne({ where: { ...options } });
+
+		return role;
+	}
+	public async create(roleDto: RoleDto): Promise<Role> {
+		return await this.roleRepository.create(roleDto);
+	}
+}
