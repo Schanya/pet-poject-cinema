a23f1a604e958e9f8573071bec1ed0114faa5086
Schanya <anyashemelevadm@gmail.com>
2023-01-21 18:35:04 +0300

    add api for schema

---
+++ src/core/schema/schema.controller.ts
@@ -0,0 +1,37 @@
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Post,
+	Put,
+} from '@nestjs/common';
+
+import { SchemaDto } from './dto/schema.dto';
+import { SchemaService } from './schema.service';
+
+@Controller('Schema')
+export class SchemaController {
+	constructor(readonly schemaService: SchemaService) {}
+
+	@Get()
+	async getAll() {
+		return await this.schemaService.findBy({});
+	}
+
+	@Post()
+	async create(@Body() schemaDto: SchemaDto) {
+		return await this.schemaService.create(schemaDto);
+	}
+
+	@Put(':id')
+	async update(@Param('id') id: number, @Body() schemaDto: SchemaDto) {
+		return await this.schemaService.update(id, schemaDto);
+	}
+
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		return await this.schemaService.delete(id);
+	}
+}
