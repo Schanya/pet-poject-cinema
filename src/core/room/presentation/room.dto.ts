7f4dd384100c1c71ed50b619f30acafe439ac7c6
Schanya <anyashemelevadm@gmail.com>
2023-02-06 12:26:23 +0300

    correction for room module

--- src/core/room/presentation/room.dto.ts
+++ src/core/room/presentation/room.dto.ts
@@ -7,7 +7,6 @@ import {
 	IsString,
 	MaxLength,
 } from 'class-validator';
-import { Min } from 'sequelize-typescript';
 
 export class RoomDto {
 	@IsNotEmpty()
@@ -23,4 +22,20 @@ export class RoomDto {
 	@IsInt({ each: true })
 	@Transform((movieId) => Number(movieId))
 	movieId: number;
+}
+
+export class RoomOptions {
+	@IsInt()
+	id?: number;
+
+	@MaxLength(255)
+	@IsString()
+	name?: string;
+
+	@IsNumber()
+	numberOfUsers?: number;
+
+	@IsInt({ each: true })
+	@Transform((movieId) => Number(movieId))
+	movieId?: number;
 }
